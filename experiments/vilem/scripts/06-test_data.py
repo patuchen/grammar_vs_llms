92c3a3dac6e3dd6a4935ccb4db99b0e6843b8c59
Vilém Zouhar <vilem.zouhar@gmail.com>
2025-02-25 15:52:34 +0100

    transition data loader to use subset2evaluate

---
+++ experiments/vilem/scripts/06-test_data.py
@@ -0,0 +1,20 @@
+# %%
+
+import grammar_v_mtllm
+import collections
+
+data = grammar_v_mtllm.utils.load_data(split="micro_test", langs="en-cs")
+assert len(data) == 10
+data = grammar_v_mtllm.utils.load_data(split="tiny_test", langs="en-cs")
+assert len(data) == 100
+data = grammar_v_mtllm.utils.load_data(split="tiny_test", langs="all")
+assert len(data) == 1100 # 100 per each language
+
+# %%
+
+data_by_lang = collections.defaultdict(list)
+data = grammar_v_mtllm.utils.load_data(split="all", langs="all")
+for line in data:
+    data_by_lang[line["langs"]].append(line)
+
+print({k: len(v) for k,v in data_by_lang.items()})
