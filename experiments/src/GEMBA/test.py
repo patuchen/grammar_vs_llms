5a4d422efdd4e3a13305f3b3de2d0ac52c22344c
seth@manx.net <seth@manx.net>
2025-02-26 19:20:20 +0100

    update data format for GEMBA

--- experiments/src/GEMBA/test.py
+++ experiments/src/GEMBA/test.py
@@ -16,52 +16,51 @@ from gemba.utils import CODE_MAP
 
 flags.DEFINE_string('method', "GEMBA-MQM", 'Which prompt to use?')
 flags.DEFINE_string('model', "gpt-4", 'OpenAI model')
-flags.DEFINE_string('source', None, 'Filepath to the source file.')
-flags.DEFINE_string('hypothesis', None, 'Filepath to the translation file.')
 flags.DEFINE_string('lp', None, 'Language pair.')
-# flags.DEFINE_string('perturbed_prompt', None, 'Type of prompt.')
-
 flags.DEFINE_string('subset', "micro_test", 'Subset to use.')
-
 
 
 def main(argv):
     FLAGS = flags.FLAGS
 
-    data = grammar_v_mtllm.utils.load_data(split="micro_test")
-    pprint(data[0])
+    data = grammar_v_mtllm.utils.load_data(split=FLAGS.subset)
 
-    # answers = get_gemba_scores(source, hypothesis, FLAGS.source_lang, FLAGS.target_lang, FLAGS.method, FLAGS.model)
-
-    cache = dc.Cache(f'cache/{FLAGS.model}_{FLAGS.method}', expire=None, size_limit=int(10e10), cull_limit=0, eviction_policy='none')
+    cache = dc.Cache(f'cache/{FLAGS.model}_{FLAGS.method}_{FLAGS.subset}', expire=None, size_limit=int(10e10), cull_limit=0, eviction_policy='none')
     gptapi = GptApi()
 
     # TODO: handle noised prompts
 
-    # for each dict in data, and for each tgt in the dict, add a line to the df with tgt key as system, tgt value as target_seg, src as source_seg
     df = []
     for entry in data:
-        for tgt, system in entry.items():
+        for system, tgt in entry["tgt"].items():
             df.append({"system": system, "source_seg": entry["src"], "target_seg": tgt, "source_lang": CODE_MAP[entry["langs"].split("-")[0]], "target_lang": CODE_MAP[entry["langs"].split("-")[1]]})
-
+    print(df[-1])
     df = pd.DataFrame(df)
     df["prompt"] = df.apply(lambda x: apply_template(prompts[FLAGS.method]['prompt'], x), axis=1)
     parse_answer = prompts[FLAGS.method]["validate_answer"]
     answers = gptapi.bulk_request(df, FLAGS.model, parse_answer, cache=cache, max_tokens=500)
 
-    # additional step in utils
-    # list(pd.DataFrame(answers)['answer'])
+    # answer format:
+    #     {
+    #     "temperature": temperature,
+    #     "answer_id": answer_id,
+    #     "answer": answer,
+    #     "prompt": prompt,
+    #     "finish_reason": finish_reason,
+    #     "model": model,
+    # }
 
-    for answer in answers:
-        print(answer)
 
     # update data dict with scores
     for i, answer in enumerate(answers):
         data[i]['scores'][answer['system']][f'GEMBA_{FLAGS.model}_{FLAGS.method}'] = answer['answer']
 
+    # TODO: save temperature? = number of reruns
+
     # save answers to file
-    with open(f"scores/answers_{FLAGS.method}_{FLAGS.model}_{FLAGS.subset}.json", "w") as f:
-        json.dump(answers, f)
+    with open(f"scores/answers_{FLAGS.method}_{FLAGS.model}_{FLAGS.subset}.jsonl", "w") as f:
+        for answer in answers:
+            f.write(json.dumps(answer, ensure_ascii=False) + "\n")
 
 if __name__ == "__main__":
     app.run(main)
