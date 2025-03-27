2074948d1f0a6a9595e1e395ff8a6d77c4adc407
Katharina Hämmerl <haemmerl@cis.lmu.de>
2025-03-27 16:55:36 +0100

    adjust run_experiments.sh with full list of scenarios to run

--- experiments/src/run_experiments.sh
+++ experiments/src/run_experiments.sh
@@ -2,11 +2,11 @@
 
 set -x
 
-MODELS=("utter-project/EuroLLM-9B-Instruct" "Unbabel/TowerInstruct-7B-v0.2")
-SCENARIOS=("--prompt base" "--prompt minimal" "--prompt base --perturbation orthographic_over_p")
+MODELS=("utter-project/EuroLLM-9B-Instruct") # "Unbabel/TowerInstruct-7B-v0.2")
+SCENARIOS=("--prompt base" "--prompt minimal" "--prompt base --perturbation orthographic" "--prompt base --perturbation typos_synthetic" "--prompt base --perturbation L2" "--prompt base --perturbation LazyUser" "--prompt base --perturbation llm" "--prompt base --perturbation lexicalphrasal" "--prompt base --perturbation register")
 
 for model in "${MODELS[@]}"; do
   for scenario in "${SCENARIOS[@]}"; do
-    python -m main --model "${model}" ${scenario} --split micro_test --mem_percent 0.9 --gpus 2
+    python -m main --model "${model}" ${scenario} --split test --mem_percent 0.9 --gpus 1
   done
 done
